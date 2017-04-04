9a0221571becd509117efd7c08c86d698d1c0c75
kevivmatrix <vivek@inkoop.in>
2017-04-04 16:03:09 +0530

    table header spec test done

--- test/features/customer_admin_test.rb
+++ test/features/customer_admin_test.rb
@@ -3,12 +3,15 @@ require "test_helper"
 feature "CustomerAdmin" do
   scenario "Index should list only required fields" do
     visit root_path
-    page.must_have_content "Customers"
-    page.must_have_content "Name"
-    page.must_have_content "Email"
-    page.must_have_content "Gender"
-    page.must_have_content "Phone"
-    page.must_have_content "City"
-    page.must_have_content "Country"
+    table_header = page.find("#index_table_customers thead")
+    table_header.must_have_content "Name"
+    table_header.must_have_content "Email"
+    table_header.must_have_content "Gender"
+    table_header.must_have_content "Phone"
+    table_header.must_have_content "City"
+    table_header.must_have_content "Country"
+    table_header.wont_have_content "Id"
+    table_header.wont_have_content "Address"
+    table_header.wont_have_content "Zip Code"
   end
 end
