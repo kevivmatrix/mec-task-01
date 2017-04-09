b191a07948a9804f1062be5ecf200e9057ab2dba
Vivek <vivekpatna52@gmail.com>
2017-04-09 12:05:01 +0530

    readme updated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,17 +11,24 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
+# TODO - Move to a better place
 def test_enumerized factory_symbol, field, class_constant, desired_array, same_order_required = false
 	if same_order_required
 		class_constant = class_constant.sort
 		desired_array = desired_array.sort
 	end
 	valid_checks = Array.new
-	valid_checks << (class_constant == desired_array)
+	valid_checks << ( class_constant == desired_array )
 	valid_checks += desired_array.map do |element|
-		FactoryGirl.build(factory_symbol, field => element).valid?
+		factory = FactoryGirl.build(factory_symbol)
+		case factory.send(field).class.to_s
+		when "Enumerize::Set"
+			factory.send(field) << element
+		when "Enumerize::Value"
+			factory.send("#{field}=", element)
+		end
+		factory.valid?
 	end
 	valid_checks << FactoryGirl.build(factory_symbol, field => Faker::Lorem.word).invalid?
-	byebug
 	valid_checks.inject(:&)
 end
