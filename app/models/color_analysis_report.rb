9efa096bd6fb956f26c099cb0f9dc54f835bf787
kevivmatrix <vivek@inkoop.in>
2017-04-26 12:15:27 +0530

    report models created

---
+++ app/models/color_analysis_report.rb
@@ -0,0 +1,15 @@
+class ColorAnalysisReport < Report
+
+	PARAMETERS = []
+
+	PARAMETERS_STORE_ACCESSOR = [ :parameters ] + PARAMETERS
+
+	store_accessor *PARAMETERS_STORE_ACCESSOR
+
+	def data_for_csv
+		CSV.generate do |csv|
+		  
+		end
+	end
+
+end
