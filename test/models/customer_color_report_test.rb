0f30876c34c4f9b8b38b1cfff7f2266022b40f89
kevivmatrix <vivek@inkoop.in>
2017-04-28 16:00:28 +0530

    customer color report test added

--- test/models/customer_color_report_test.rb
+++ test/models/customer_color_report_test.rb
@@ -15,7 +15,14 @@ class CustomerColorReportTest < ActiveSupport::TestCase
 	end
 
 	test "Data for csv" do
-		
+		customer_1 = FactoryGirl.create :customer, favorite_colors: ["red", "orange"]
+		customer_2 = FactoryGirl.create :customer, favorite_colors: ["yellow", "green"]
+		customer_color_report = FactoryGirl.create :customer_color_report
+
+		csv_data = customer_color_report.data_for_csv
+		csv_data_lines = csv_data.split("\n")
+
+		assert_equal csv_data_lines
 	end
 
 end
