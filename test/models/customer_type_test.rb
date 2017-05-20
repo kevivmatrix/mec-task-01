171ad33fbfc168e0fa1c193a483644f8a3fde046
Vivek <vivekpatna52@gmail.com>
2017-05-20 20:07:27 +0530

    customer type test fix

--- test/models/customer_type_test.rb
+++ test/models/customer_type_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe CustomerType do
-  let(:customer_type) { CustomerType.new }
+  let(:customer_type) { FactoryGirl.create :customer_type }
 
   it "must be valid" do
     value(customer_type).must_be :valid?
