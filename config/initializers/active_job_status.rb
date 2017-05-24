8cafc1224df663f05a3e3481511f520a6efea884
kevivmatrix <vivek@inkoop.in>
2017-05-24 11:31:15 +0530

    redis cache fix

--- config/initializers/active_job_status.rb
+++ config/initializers/active_job_status.rb
@@ -1,3 +1,5 @@
 # ActiveJobStatus.store = ActiveSupport::Cache::MemoryStore.new
 # ActiveJobStatus.store = ActiveSupport::Cache::RedisStore.new
-ActiveJob::Status.store = :redis_store
+# ActiveJob::Status.store = Rails.cache
+ActiveJob::Status.store = ActiveSupport::Cache::RedisStore.new("#{ENV['REDISTOGO_URL'] || "redis://localhost:6379/"}0/cache")
+# ActiveJob::Status.store = :redis_store
